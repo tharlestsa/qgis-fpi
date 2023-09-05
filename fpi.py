ad24e199cd918dddfc618468f4b01e7f6942d1f7
Tharles de Sousa Andrade <irtharles@gmail.com>
2023-09-05 17:20:10 -0300

    handle some errors.

--- fpi.py
+++ fpi.py
@@ -9,98 +9,98 @@ from PyQt5.QtWidgets import QDockWidget, QVBoxLayout, QWidget, QLabel, QPushButt
 
 # Your classes
 CLASSES = [
-	{
-		"class": "Afloramento Rochoso",
-		"rgba": "255,170,95,77"
-	},
-	{
-		"class": "Agricultura",
-		"rgba": "233,116,237,77"
-	},
-	{
-		"class": "Apicum",
-		"rgba": "252,129,20,77"
-	},
-	{
-		"class": "Aquicultura",
-		"rgba": "9,16,119,77"
-	},
-	{
-		"class": "Campo Alagado e Área Pantanosa",
-		"rgba": "81,151,153,77"
-	},
-	{
-		"class": "Floresta Alagável (beta)",
-		"rgba": "2,105,117,77"
-	},
-	{
-		"class": "Formação Campestre",
-		"rgba": "214,188,116,77"
-	},
-	{
-		"class": "Formação Florestal",
-		"rgba": "31,141,73,77"
-	},
-	{
-		"class": "Formação Savânica",
-		"rgba": "125,201,117,77"
-	},
-	{
-		"class": "Lavoura Perene",
-		"rgba": "208,130,222,77"
-	},
-	{
-		"class": "Lavoura Temporária",
-		"rgba": "194,123,160,77"
-	},
-	{
-		"class": "Mangue",
-		"rgba": "4,56,29,77"
-	},
-	{
-		"class": "Mineração",
-		"rgba": "156,0,39,77"
-	},
-	{
-		"class": "Mosaico de Usos",
-		"rgba": "255,239,195,77"
-	},
-	{
-		"class": "Outras Formações não Florestais",
-		"rgba": "216,159,92,77"
-	},
-	{
-		"class": "Outras Áreas não Vegetadas",
-		"rgba": "219,77,79,77"
-	},
-	{
-		"class": "Pastagem",
-		"rgba": "237,222,142,77"
-	},
-	{
-		"class": "Praia, Duna e Areal",
-		"rgba": "255,160,122,77"
-	},
-	{
-		"class": "Restinga Arbórea",
-		"rgba": "2,214,89,77"
-	},
-	{
-		"class": "Restinga Herbácea",
-		"rgba": "173,81,0,77"
-	},
-	{
-		"class": "Rio, Lago e Oceano",
-		"rgba": "37,50,228,77"
-	},
-	{
-		"class": "Silvicultura",
-		"rgba": "122,89,0,77"
-	},
-	{
-		"class": "Área Urbanizada",
-		"rgba": "212,39,30,77"
-	},
+    {
+        "class": "Afloramento Rochoso",
+        "rgba": "255,170,95,77"
+    },
+    {
+        "class": "Agricultura",
+        "rgba": "233,116,237,77"
+    },
+    {
+        "class": "Apicum",
+        "rgba": "252,129,20,77"
+    },
+    {
+        "class": "Aquicultura",
+        "rgba": "9,16,119,77"
+    },
+    {
+        "class": "Campo Alagado e Área Pantanosa",
+        "rgba": "81,151,153,77"
+    },
+    {
+        "class": "Floresta Alagável (beta)",
+        "rgba": "2,105,117,77"
+    },
+    {
+        "class": "Formação Campestre",
+        "rgba": "214,188,116,77"
+    },
+    {
+        "class": "Formação Florestal",
+        "rgba": "31,141,73,77"
+    },
+    {
+        "class": "Formação Savânica",
+        "rgba": "125,201,117,77"
+    },
+    {
+        "class": "Lavoura Perene",
+        "rgba": "208,130,222,77"
+    },
+    {
+        "class": "Lavoura Temporária",
+        "rgba": "194,123,160,77"
+    },
+    {
+        "class": "Mangue",
+        "rgba": "4,56,29,77"
+    },
+    {
+        "class": "Mineração",
+        "rgba": "156,0,39,77"
+    },
+    {
+        "class": "Mosaico de Usos",
+        "rgba": "255,239,195,77"
+    },
+    {
+        "class": "Outras Formações não Florestais",
+        "rgba": "216,159,92,77"
+    },
+    {
+        "class": "Outras Áreas não Vegetadas",
+        "rgba": "219,77,79,77"
+    },
+    {
+        "class": "Pastagem",
+        "rgba": "237,222,142,77"
+    },
+    {
+        "class": "Praia, Duna e Areal",
+        "rgba": "255,160,122,77"
+    },
+    {
+        "class": "Restinga Arbórea",
+        "rgba": "2,214,89,77"
+    },
+    {
+        "class": "Restinga Herbácea",
+        "rgba": "173,81,0,77"
+    },
+    {
+        "class": "Rio, Lago e Oceano",
+        "rgba": "37,50,228,77"
+    },
+    {
+        "class": "Silvicultura",
+        "rgba": "122,89,0,77"
+    },
+    {
+        "class": "Área Urbanizada",
+        "rgba": "212,39,30,77"
+    }
 ]
 
 
@@ -115,69 +115,78 @@ class LayerSelectionDockWidget(QDockWidget):
         self.init(classes)
 
     def init(self, classes):
-        self.setWindowTitle("Fast Point Inspection")
-        self.classes = classes
-        self.cls_selected = None
-        self.progress_message_bar = None
-        self.progress_bar = None
-        self.widget = QWidget()
-        self.setWidget(self.widget)
+        try:
+            self.setWindowTitle("Fast Point Inspection")
+            self.classes = classes
+            self.cls_selected = None
+            self.progress_message_bar = None
+            self.progress_bar = None
+            self.widget = QWidget()
+            self.setWidget(self.widget)
 
-        layout = QVBoxLayout()
+            layout = QVBoxLayout()
 
-        # Get the point layer from the project
-        self.point_layer = self.get_point_layer()
+            # Get the point layer from the project
+            self.point_layer = self.get_point_layer()
 
-        if self.point_layer is None:
+            if self.point_layer is None:
+                iface.messageBar().pushMessage(
+                    'POINTS GRID',
+                    'The grid with points was not found.',
+                    level=Qgis.Critical,
+                    duration=5,
+                )
+                return
+
+            if self.point_layer is not None:
+                self.zoom()
+                self.add_class_field()
+
+            # Label to show the current selected class
+            self.current_class_label = QLabel("Class not selected")
+            self.current_class_label.setAlignment(Qt.AlignCenter)
+            self.current_class_label.setStyleSheet("font-size: 25px;")
+            layout.addWidget(self.current_class_label)
+            self.set_feature_color()
+
+            # Class buttons with grid layout
+            self.class_buttons = []
+            class_layout = QGridLayout()
+            row, col = 0, 0
+            for cls in classes:
+                btn = QPushButton(cls["class"])
+                btn.setStyleSheet(f"background-color: rgb({','.join(cls['rgba'].split(',')[:3])})")
+                btn.clicked.connect(lambda _, cls=cls: self.on_class_selected(cls))
+                btn.setCursor(Qt.PointingHandCursor)
+                self.class_buttons.append(btn)
+
+                class_layout.addWidget(btn, row, col)
+                col += 1
+                if col >= 2:  # Set the number of columns in the grid
+                    col = 0
+                    row += 1
+            self.point_layer.selectionChanged.connect(self.set_class_for_selected_features)
+
+            # Clear classification button
+            clear_btn = QPushButton("Clear Classification")
+            clear_btn.clicked.connect(self.clear_classification)
+            clear_btn.setCursor(Qt.PointingHandCursor)
+
+            layout.addLayout(class_layout)
+            layout.addWidget(clear_btn)
+
+            self.widget.setLayout(layout)
+
+            # Add the dock widget to the QGIS interface
+            iface.addDockWidget(Qt.RightDockWidgetArea, self)
+        except Exception as e:
             iface.messageBar().pushMessage(
-                'POINTS GRID',
-                'The grid with points was not found.',
+                'init',
+                f"Error -> {e}",
                 level=Qgis.Critical,
                 duration=5,
             )
-            return
-
-        if self.point_layer is not None:
-            self.zoom()
-            self.add_class_field()
-
-        # Label to show the current selected class
-        self.current_class_label = QLabel("Class not selected")
-        self.current_class_label.setAlignment(Qt.AlignCenter)
-        self.current_class_label.setStyleSheet("font-size: 25px;")
-        layout.addWidget(self.current_class_label)
-        self.set_feature_color()
-
-        # Class buttons with grid layout
-        self.class_buttons = []
-        class_layout = QGridLayout()
-        row, col = 0, 0
-        for cls in classes:
-            btn = QPushButton(cls["class"])
-            btn.setStyleSheet(f"background-color: rgb({','.join(cls['rgba'].split(',')[:3])})")
-            btn.clicked.connect(lambda _, cls=cls: self.on_class_selected(cls))
-            btn.setCursor(Qt.PointingHandCursor)
-            self.class_buttons.append(btn)
-
-            class_layout.addWidget(btn, row, col)
-            col += 1
-            if col >= 2:  # Set the number of columns in the grid
-                col = 0
-                row += 1
-        self.point_layer.selectionChanged.connect(self.set_class_for_selected_features)
-
-        # Clear classification button
-        clear_btn = QPushButton("Clear Classification")
-        clear_btn.clicked.connect(self.clear_classification)
-        clear_btn.setCursor(Qt.PointingHandCursor)
-
-        layout.addLayout(class_layout)
-        layout.addWidget(clear_btn)
-
-        self.widget.setLayout(layout)
-
-        # Add the dock widget to the QGIS interface
-        iface.addDockWidget(Qt.RightDockWidgetArea, self)
+            pass
 
     def reset(self):
         self.classes = None
@@ -187,26 +196,34 @@ class LayerSelectionDockWidget(QDockWidget):
         self.widget = None
 
     def start_processing(self):
-        # Remove existing progress bar if any
-        if self.progress_message_bar:
-            self.finish_progress()
+        try:
+            # Remove existing progress bar if any
+            if self.progress_message_bar:
+                self.finish_progress()
 
-        self.progress_message_bar = iface.messageBar().createMessage("")
+            self.progress_message_bar = iface.messageBar().createMessage("")
 
-        self.progress_bar = QProgressBar()
-        self.progress_bar.setMaximum(100)
+            self.progress_bar = QProgressBar()
+            self.progress_bar.setMaximum(100)
 
-        # Create a QWidget to hold the progress bar
-        container = QWidget()
-        layout = QHBoxLayout(container)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(QLabel("Processing..."))
-        layout.addWidget(self.progress_bar)
-        container.setLayout(layout)
+            # Create a QWidget to hold the progress bar
+            container = QWidget()
+            layout = QHBoxLayout(container)
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.addWidget(QLabel("Processing..."))
+            layout.addWidget(self.progress_bar)
+            container.setLayout(layout)
 
-        self.progress_message_bar.layout().addWidget(container)
-        iface.messageBar().pushWidget(self.progress_message_bar, Qgis.Info)
-
+            self.progress_message_bar.layout().addWidget(container)
+            iface.messageBar().pushWidget(self.progress_message_bar, Qgis.Info)
+        except Exception as e:
+            iface.messageBar().pushMessage(
+                'start_processing',
+                f"Error -> {e}",
+                level=Qgis.Critical,
+                duration=5,
+            )
+            pass
     def update_progress(self, value):
         if self.progress_bar:
             self.progress_bar.setValue(value)
@@ -222,17 +239,26 @@ class LayerSelectionDockWidget(QDockWidget):
             self.progress_message_bar = None
 
     def zoom(self):
-        # Set the extent to the layer's extent
-        iface.mapCanvas().setExtent(self.point_layer.extent())
-        self.point_layer.extent()
+        try:
+            # Set the extent to the layer's extent
+            iface.mapCanvas().setExtent(self.point_layer.extent())
+            self.point_layer.extent()
 
-        # Set the scale to 1:5000
-        iface.mapCanvas().zoomScale(5000)
+            # Set the scale to 1:5000
+            iface.mapCanvas().zoomScale(5000)
 
-        iface.mapCanvas().setDestinationCrs(self.point_layer.crs())
+            iface.mapCanvas().setDestinationCrs(self.point_layer.crs())
 
-        # Refresh the canvas
-        iface.mapCanvas().refreshAllLayers()
+            # Refresh the canvas
+            iface.mapCanvas().refreshAllLayers()
+        except Exception as e:
+            iface.messageBar().pushMessage(
+                'ON_SET_CLASS',
+                "An error occurred while attempting to save the class.",
+                level=Qgis.Critical,
+                duration=5,
+            )
+            pass
 
     def clear_classification(self):
         if self.point_layer is None:
